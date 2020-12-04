1108fbd70e113a95e3637b75f05afaaa02ede5b2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-12-04 13:42:25 +0100

    Add new addresses

--- packages/ui/src/constants/addresses.ts
+++ packages/ui/src/constants/addresses.ts
@@ -3,3 +3,7 @@ import { ChainId } from './chainId'
 export const MULTICALL_ADDRESS = {
   [ChainId.Ganache]: '0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0',
 }
+
+export const DEX_ADDRESS = {
+  [ChainId.Ganache]: '0xf25186B5081Ff5cE73482AD761DB0eB0d25abfBF'
+}
--- packages/ui/src/constants/currencies.ts
+++ packages/ui/src/constants/currencies.ts
@@ -2,7 +2,10 @@ import { Currency, Token } from '../model'
 import { ChainId } from './chainId'
 
 export const GanacheEther = new Currency(ChainId.Ganache, 'Ganache Ether', 'GETH', 18)
+export const Janusz = new Token(ChainId.Ganache, '0xF12b5dd4EAD5F743C6BaA640B0216200e89B60Da', 'Janusz', 'JAN', 18)
+export const Grazyna = new Token(ChainId.Ganache, '0x345cA3e014Aaf5dcA488057592ee47305D9B3e10', 'Grazyna', 'GRA', 18)
 
 export const NATIVE_CURRENCY = {
   [ChainId.Ganache]: GanacheEther,
 }
+
